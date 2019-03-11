6231c96a818cf702ef42f1ce4d895065f0ecda3d
Oscar Strandmark <oscar.strandmark1@gmail.com>
2019-03-11 16:18:51 +0100

    ContactWindow done (?)

--- Client/UI/ContactWindow.java
+++ Client/UI/ContactWindow.java
@@ -3,14 +3,18 @@ package UI;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+import javax.swing.ListModel;
 import javax.swing.JList;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
 import client.Controller;
+import shared.User;
 
 import javax.swing.JButton;
 import java.awt.Font;
@@ -19,27 +23,34 @@ public class ContactWindow {
 
 	private JFrame frame = new JFrame("Contacts");
 	private JScrollPane scrollPane = new JScrollPane();
-	private JList listContacts = new JList();
+	private JScrollPane scrollPane_1 = new JScrollPane();
+
+	private JList<String> listContacts = new JList<String>();
 	private JLabel lblKontakter = new JLabel("Contacts");
-	private JButton btnClose = new JButton("Close");
-	private final JScrollPane scrollPane_1 = new JScrollPane();
-	private final JLabel lblAnslutnaAnvndare = new JLabel("Connected Users");
-	private final JList listUsers = new JList();
+
+	private JLabel lblAnslutnaAnvndare = new JLabel("Connected Users");
+	private JList<String> listUsers = new JList<String>();
+
+	private HashMap<String, User> userMap;
+
 	private JButton btnFromContacts = new JButton("----->");
 	private JButton btnFromUsers = new JButton("<-----");
+	private JButton btnClose = new JButton("Save and Close");
 
 	private Controller controller;
 
+	private ArrayList<String> contacts;
 
 	public ContactWindow(Controller controller) {
 		this.controller = controller;
 		initialize();
+		initializeLists();
 	}
 
 	private void initialize() {
 		frame.setVisible(true);
 		frame.setBounds(100, 100, 830, 683);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		Listener listener = new Listener();
@@ -57,14 +68,13 @@ public class ContactWindow {
 		btnClose.setBounds(300, 529, 205, 74);
 		frame.getContentPane().add(btnClose);
 		scrollPane_1.setBounds(511, 83, 268, 389);
-		btnClose.addActionListener(listener);		
+		btnClose.addActionListener(listener);
 		frame.getContentPane().add(scrollPane_1);
 
 		scrollPane_1.setViewportView(listUsers);
 		lblAnslutnaAnvndare.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblAnslutnaAnvndare.setBounds(572, 47, 207, 20);		
+		lblAnslutnaAnvndare.setBounds(572, 47, 207, 20);
 		frame.getContentPane().add(lblAnslutnaAnvndare);
-
 
 		btnFromContacts.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnFromContacts.setBounds(339, 197, 115, 29);
@@ -75,20 +85,121 @@ public class ContactWindow {
 		frame.getContentPane().add(btnFromUsers);
 	}
 
+	private void initializeLists() {
+		ArrayList<User> contacts = controller.getContacts();
+		ArrayList<User> connected = controller.getConnectedUsers();
+
+		userMap = new HashMap<String, User>();
+
+		for (User u : contacts) {
+			if (connected.contains(u)) {
+				connected.remove(u);
+			}
+		}
+		ArrayList<String> contactString = new ArrayList<String>();
+		ArrayList<String> connectedString = new ArrayList<String>();
+
+		for (User u : contacts) {
+			userMap.put(u.getUsername(), u);
+			contactString.add(u.getUsername());
+		}
+
+		for (User u : connected) {
+			userMap.put(u.getUsername(), u);
+			connectedString.add(u.getUsername());
+		}
+
+		listUsers = new JList<String>((String[]) contactString.toArray());
+		listContacts = new JList<String>((String[]) contactString.toArray());
+		listUsers.updateUI();
+		listContacts.updateUI();
+	}
+
 	private class Listener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (e.getSource() == btnClose) {
+				ListModel<String> model = listContacts.getModel();
+				ArrayList<User> newContacts = new ArrayList<User>();
+				for (int i = 0; i < model.getSize(); i++) {
+					String username = model.getElementAt(i);
+					newContacts.add(userMap.get(username));
+				}
+				controller.setContacts(newContacts);
 				frame.dispose();
 			}
 
 			if (e.getSource() == btnFromContacts) {
-				
-			}
-			
-			if(e.getSource() == btnFromUsers) {
+				int selectedIndex = listContacts.getSelectedIndex();
+				String selectedUser = listContacts.getModel().getElementAt(selectedIndex);
 
+				ArrayList<User> contacts = controller.getContacts();
+				ArrayList<User> connected = controller.getConnectedUsers();
+
+				userMap = new HashMap<String, User>();
+
+				for (User u : contacts) {
+					if (connected.contains(u)) {
+						connected.remove(u);
+					}
+				}
+				ArrayList<String> contactString = new ArrayList<String>();
+				ArrayList<String> connectedString = new ArrayList<String>();
+
+				for (User u : contacts) {
+					userMap.put(u.getUsername(), u);
+					contactString.add(u.getUsername());
+				}
+
+				for (User u : connected) {
+					userMap.put(u.getUsername(), u);
+					connectedString.add(u.getUsername());
+				}
+
+				contactString.remove(selectedUser);
+				connectedString.add(selectedUser);
+
+				listUsers = new JList<String>((String[]) contactString.toArray());
+				listContacts = new JList<String>((String[]) contactString.toArray());
+				listUsers.updateUI();
+				listContacts.updateUI();
+			}
+
+			if (e.getSource() == btnFromUsers) {
+				int selectedIndex = listUsers.getSelectedIndex();
+				String selectedUser = listUsers.getModel().getElementAt(selectedIndex);
+
+				ArrayList<User> contacts = controller.getContacts();
+				ArrayList<User> connected = controller.getConnectedUsers();
+
+				userMap = new HashMap<String, User>();
+
+				for (User u : contacts) {
+					if (connected.contains(u)) {
+						connected.remove(u);
+					}
+				}
+				ArrayList<String> contactString = new ArrayList<String>();
+				ArrayList<String> connectedString = new ArrayList<String>();
+
+				for (User u : contacts) {
+					userMap.put(u.getUsername(), u);
+					contactString.add(u.getUsername());
+				}
+
+				for (User u : connected) {
+					userMap.put(u.getUsername(), u);
+					connectedString.add(u.getUsername());
+				}
+
+				contactString.add(selectedUser);
+				connectedString.remove(selectedUser);
+
+				listUsers = new JList<String>((String[]) contactString.toArray());
+				listContacts = new JList<String>((String[]) contactString.toArray());
+				listUsers.updateUI();
+				listContacts.updateUI();
 			}
 		}
 	}
--- Client/client/Controller.java
+++ Client/client/Controller.java
@@ -1,6 +1,14 @@
 package client;
 
 import java.util.List;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -19,16 +27,68 @@ public class Controller {
 	private UIHandler ui;
 	private Connection connection;
 	private ArrayList<User> connectedUsers;
-	private ArrayList<User> contacts;
 	private ArrayList<MediaMessage> messages;
 	private User me;
+
+	private static final String FILEPATH_CONTACTS = "C:\\dev\\ServerLogFile\\contacts.dat";
+	private ArrayList<User> contacts;
 
 	public Controller() {
 		connectedUsers = new ArrayList<User>();
 		connection = new Connection(SERVERADDRESS, PORT, this);
 		ui = new UIHandler(this);
 		messages = new ArrayList<MediaMessage>();
+		readContactsFromFile();
+	}
 
+	private void readContactsFromFile() {
+		File file = new File(FILEPATH_CONTACTS);
+		if (file.exists()) {
+			try (ObjectInputStream ois = new ObjectInputStream(
+					new BufferedInputStream(new FileInputStream(FILEPATH_CONTACTS)))) {
+
+				while (true) {
+					User u = (User) ois.readObject();
+					contacts.add(u);
+				}
+			} catch (EOFException e) {
+				// EOFException is always throwns after all objects are read.
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		} else {
+			file.mkdirs();
+		}
+
+	}
+
+	/**
+	 * Called when a user is added to the contacts;
+	 * 
+	 * @param user
+	 */
+	public void writeContacts(ArrayList<User> users) {
+
+		contacts.clear();
+
+		for (User u : users) {
+			contacts.add(u);
+		}
+
+		File oldContacts = new File(FILEPATH_CONTACTS);
+		oldContacts.delete();
+
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(
+					new BufferedOutputStream(new FileOutputStream(FILEPATH_CONTACTS)));
+			for (User u : users) {
+				oos.writeObject(u);
+			}
+			oos.flush();
+			oos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void sendMessage(String text, ImageIcon img, List<User> recipients) {
@@ -41,11 +101,11 @@ public class Controller {
 		connection.connect(me);
 	}
 
-	public List<User> getConnectedUsers() {
+	public ArrayList<User> getConnectedUsers() {
 		return connectedUsers;
 	}
 
-	public List<User> getContacts() {
+	public ArrayList<User> getContacts() {
 		return contacts;
 	}
 
